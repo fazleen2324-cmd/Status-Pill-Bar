df4b7e404c9c83e8cc13b06786c6c64d337452be
fazleen2324-cmd <fazleen2324@gmail.com>
2026-02-11 15:08:27 +0530

    yes

--- index.py
+++ index.py
@@ -1,10 +1,7 @@
-<<<<<<< HEAD
 print("Hello Fazleen , this is a test script for the status pill bar.")
 print("This is a test.")
-=======
 print("Hello Aleem")
 print("This is a test script.")
->>>>>>> 86740d465db989ee1b0639af216cbdb17b8dcbe7
 print("This is a test spt.")
 print("This is a pt.")
 print("This is a pt2")
